80cbf6fe82b0e7d6e0b7aa0d4afe7a0b5c5abd33
Chandan Purbia <kingpurbiaforever@gmail.com>
2019-12-27 18:01:20 +0900

    refactor: use the same code to repeat the same block

--- src/app/controller.py
+++ src/app/controller.py
@@ -4,4 +4,24 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cards = [
+        {
+            "func": "Add",
+            "title": "Add a New Customer",
+            "content": "To add a new customer. " +
+                       "Please enter all the details for future use. " +
+                       "Make sure to confirm before adding."
+        },
+        {
+            "func": "View/Edit",
+            "title": "View All Customer",
+            "content": "To view all the details of the customers added. " +
+                       "Also you can select and edit an existing customer's details."
+        },
+        {
+            "func": "Edit",
+            "title": "Edit a Customer",
+            "content": "To edit an existing customer's details."
+        }
+    ]
+    return render_template('index.html', cards=cards)
