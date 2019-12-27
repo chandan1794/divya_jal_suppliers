e909bd5acebda7cc4f9a559de8bf84e409e5b57a
Chandan Purbia <kingpurbiaforever@gmail.com>
2019-12-27 19:07:24 +0900

    refactor: add more cards to homepage

--- src/app/controller.py
+++ src/app/controller.py
@@ -6,6 +6,18 @@ from flask import render_template
 def index():
     cards = [
         {
+            "func": "View",
+            "title": "View All Pending Orders",
+            "content": "To see all the pending orders which needs to be " +
+                       "completed TODAY."
+        },
+        {
+            "func": "View",
+            "title": "View All Pending Collections",
+            "content": "To see all the pending collections which needs to " +
+                       "be collected"
+        },
+        {
             "func": "Add",
             "title": "Add a New Customer",
             "content": "To add a new customer. " +
@@ -21,6 +33,23 @@ def index():
         {
             "func": "Edit",
             "title": "Edit a Customer",
+            "content": "To edit an existing customer's details."
+        },
+        {
+            "func": "Add",
+            "title": "Add a Need",
+            "content": "To add need of an existing customer. A need is what customer "
+                       "wants? How many kettles? How frequently? On which days? etc."
+        },
+        {
+            "func": "View/Edit",
+            "title": "View All Needs",
+            "content": "To view all the details of the needs added. " +
+                       "Also you can select and edit an existing need's details."
+        },
+        {
+            "func": "Edit",
+            "title": "Edit a Need",
             "content": "To edit an existing customer's details."
         }
     ]
